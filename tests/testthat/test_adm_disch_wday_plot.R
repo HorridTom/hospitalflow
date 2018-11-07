c7e1e67f8696ab5b620c321be7e1a7d4a5e4c689
Burcea <gburcea@ic.ac.uk>
2018-11-07 15:31:12 +0000

    replaced the new function admission_discharges_jan_jun back to the original function admission_discharges function. The test failed as expected since I had the new tibble with dates from Jan to Jun(in the original function admission_discharges function I had a tibble column with only 1 month of dates for Jan).

--- tests/testthat/test_adm_disch_wday_plot.R
+++ tests/testthat/test_adm_disch_wday_plot.R
@@ -38,3 +38,40 @@ test_that("Admission numbers by day of the Week for January 2015",{
  })
 
 
+test_that("Admission numbers by day of the Week for January-Jun 2015",{
+
+  load("testdata/test_jan_jun_data.rda")
+
+  #Specify correct results
+  correct_answers_jan_jun <- tibble::tibble(
+    Weekday = as.character(c("Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat")),
+
+    Event =  c("Avg_admissions", "Avg_admissions","Avg_admissions", "Avg_admissions","Avg_admissions","Avg_admissions","Avg_admissions",
+               "Avg_discharges","Avg_discharges","Avg_discharges","Avg_discharges","Avg_discharges","Avg_discharges","Avg_discharges",
+               "Non_emergency_admissions","Non_emergency_admissions","Non_emergency_admissions","Non_emergency_admissions","Non_emergency_admissions",
+               "Non_emergency_admissions","Non_emergency_admissions"),
+
+    Value = as.numeric(c(1.92, 3.23, 3.38, 3.48, 3.46, 2.88, 1.96, 2.96, 2.42, 2.46, 3.16, 3.38, 3.85, 2.54, 0.04, 0.08, 0.19, 0.16, 0.15, 0.08, 0.12)))
+
+  correct_answers_jan_jun <- correct_answers_jan_jun %>%
+    dplyr::arrange(Event, Weekday)
+
+  #Run Admission Discharges graph
+
+  result_jan_jun <- admission_discharges(start_date = "2015-01-01 00:00:00", end_date = "2015-07-01 00:00:00", data = adm_disch_jan_jun, plot_chart = TRUE)
+
+  result_data_jj <- result_jan_jun$data
+  #result_data_jj$Weekday <- as.character(result_data_jj$Weekday)
+
+
+  result_jan_jun <- result_data_jj %>%
+    dplyr::mutate(Weekday = as.character(Weekday)) %>%
+    dplyr::arrange(Event, Weekday)
+
+  #Test results are correct
+  expect_equal(as.data.frame(result_jan_jun), as.data.frame(correct_answers_jan_jun), tolerance = 0.01)
+
+
+})
+
+
