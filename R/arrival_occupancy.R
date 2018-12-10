2a6e108e17f9a93ffcc256f1993ae5f987728257
Burcea <gburcea@ic.ac.uk>
2018-12-10 17:28:41 +0000

    arranges lines for clarity of the code.

--- R/arrival_occupancy.R
+++ R/arrival_occupancy.R
@@ -9,48 +9,97 @@ arrival_occupancy <- function(start_date, end_date, data, plot_chart){
   # extracting Month and hour from Time column and
   # count = occupancy.
   # ASK TOM FOR THE AVERAGES - WE GET DIFFERENT AVERAGES DEPENDENT OF THE GROUP BY IN DIFFERENT CASES - GROUP_BY WITH DAY AND HOUR GIVES DIFFERENT AVERAGES
-  dt_occ_count <- data %>%
+  # using gather function to create a new column with date; and filter only by Emergency Department
+  arrivals_occupancy_jan_march <-  test_data %>%
+    tidyr::gather(key = Type, Time, Admissions:Discharges) %>%
+    dplyr::filter(PatientType == "Emergency" & EpisodeNumber == 1)
+
+
+  # extracting year, month, day and hour from Time column and count = occupancy
+  dt_sep <- arrivals_occupancy_jan_march %>%
     dplyr::mutate(
       Time = lubridate::ymd_hms(Time),
-      Month = lubridate::month(Time),
-      Hour = lubridate::hour(Time)
-    ) %>%
-    dplyr::count(Month, Hour)
+      Day = lubridate::day(Time),
+      Hour = lubridate::hour(Time)) %>%
+    dplyr::group_by(Day, Hour) %>%
+    dplyr::tally() %>%
+    dplyr::rename(Occupancy = n)
 
-  # getting the averages
-  tbl_avg_occ <- dt_occ_count %>%
+
+  start_date <- as.POSIXct("2015-01-01 00:00:00", tz = "Europe/London")
+  end_date <- as.POSIXct( "2015-04-01 00:00:00", tz = "Europe/London")
+
+
+  #This format is more flexible than the one I suggested
+  df_period <- seq.POSIXt(from = as.POSIXct(start_date, tz = "Europe/London"),
+                          to = as.POSIXct(end_date, tz= "Europe/London"),
+                          by = "hour")
+  #df_period
+  #Place the new list, with dates, into a tibble and format it accordingly.
+  # DO the same as with the adm column by finding the weekday, and merged with the admission table
+
+  df_adm_date <- tibble::as.tibble(df_period)
+
+  df_date_adm_ren <- df_adm_date %>%
+    dplyr::rename(Time = "value") %>%
+    dplyr::mutate(Day = lubridate::day(Time),
+                  Hour = lubridate::hour(Time)) %>%
+    dplyr::select(Day, Hour)
+
+
+  table_average_occ <- dplyr::full_join(dt_sep, df_date_adm_ren, by = c("Day", "Hour"))
+
+
+  dt_occ_final <- table_average_occ %>%
+    tidyr::replace_na(list(Occupancy  = 0))
+
+
+  tbl_avg_occ <- dt_occ_final %>%
     dplyr::group_by(Hour) %>%
-    dplyr::summarize(Average_occ = round(mean(n), digits = 2)) %>%
-    dplyr::ungroup()
+    dplyr::summarize(Average_occ = round(mean(Occupancy), digits = 2))
+
+
+  # extracting year, month, day and hour from Time column and count = occupancy
+  dt_sep <- arrivals_occupancy_jan_march %>%
+    dplyr::mutate(
+      Time = lubridate::ymd_hms(Time),
+      Day = lubridate::day(Time),
+      Hour = lubridate::hour(Time)) %>%
+    dplyr::group_by(Day, Hour) %>%
+    dplyr::tally() %>%
+    dplyr::rename(Occupancy = n)
+
 
   # take out the start-datetime only -> this will be used later to be added to a plot alongside the occupancy
-   # Count the Arrivals in A&E
-  dt_arriv_count <- data %>%
+
+  dt_sep_arrivals <- arrivals_occupancy_jan_march  %>%
     dplyr::filter(Type == "Admissions") %>%
+    dplyr::select(IDcol, Time) %>%
     dplyr::mutate(
-      Month = lubridate::month(Time),
-      Hour = lubridate::hour(Time)
-    ) %>%
-    dplyr::count(Month, Hour)#Month,
+      Day = lubridate::day(Time),
+      Hour = lubridate::hour(Time)) %>%
+    dplyr::group_by(Day, Hour) %>%
+    dplyr::tally() %>%
+    dplyr::rename(Arrivals = n)
 
-  # Get averages
-  tbl_avg_arrivals <- dt_arriv_count %>%
+
+  table_average_arrivals <- dplyr::full_join(dt_sep_arrivals, df_date_adm_ren, by = c("Day", "Hour"))
+
+  dt_avg_arrivals <- table_average_arrivals %>%
+    tidyr::replace_na(list(Arrivals  = 0))
+
+  tbl_avg_arrivals <- dt_avg_arrivals %>%
     dplyr::group_by(Hour) %>%
-    dplyr::summarize(Average_arrivals = round(mean(n), digits = 2)) %>%
-    dplyr::ungroup()
+    dplyr::summarize(Average_arrivals = round(mean(Arrivals), digits = 2))
 
-
-  # Left join the two tables
   tbl_occ_arriv <- dplyr::left_join(tbl_avg_arrivals, tbl_avg_occ, by = "Hour")
 
-
-  # Get to plot - Arrivals vs. Occupancy
-  plt_arriv_occ <- ggplot2::ggplot(tbl_occ_arriv, ggplot2::aes(x = as.numeric(Hour), y = Average_occ, group = Hour)) +
+  plt_occ_percent <- ggplot2::ggplot(tbl_occ_arriv, ggplot2::aes(x = as.numeric(Hour), y = Average_occ, group = Hour)) +
     ggplot2::geom_bar(stat = "identity", alpha=0.7, width = 0.50, ggplot2::aes(fill = "Occupancy")) +
-    ggplot2::scale_x_continuous(breaks = 0:23, expand = c(0, .2)) +
-    ggplot2::scale_y_continuous(expand = c(0, .5)) + # breaks = c(0, 5, 10, 15, 20, 25))
-    ggplot2::geom_point(ggplot2::aes(y = Average_arrivals)) +
-    ggplot2::geom_line(ggplot2::aes(y = Average_arrivals, group = 1, color = "Arrivals in A&E")) +
+    ggplot2::scale_x_continuous(breaks = 0:23, expand = c(0, 0.2)) + #, expand = c(0, 0)
+    #ggplot2::scale_y_continuous(expand = c(0, 0)) + # breaks = c(0, 5, 10, 15, 20, 25))
+    ggplot2::geom_point(aes(y = Average_arrivals)) +
+    ggplot2::geom_line(aes(y = Average_arrivals, group = 1, color = "Arrivals in A&E")) +
     ggplot2::scale_fill_manual("",values="yellow4") +
     ggplot2::scale_color_manual("",values = 1) +
     ggplot2::theme_bw() +
@@ -62,8 +111,7 @@ arrival_occupancy <- function(start_date, end_date, data, plot_chart){
                    plot.subtitle = ggplot2::element_text(size = 10),
                    legend.position = "bottom", legend.box = "horizontal")
 
-
-  plt_arriv_occ
+  plt_occ_percent
 
   if(plot_chart == TRUE){
 
