6f298dbfe2bc19293b58ff36cf86bbe4cfcd01e9
Burcea <gburcea@ic.ac.uk>
2018-12-17 16:23:48 +0000

    added new simple test

--- tests/testthat/test_arrival_occupancy_hourday_plot.R
+++ tests/testthat/test_arrival_occupancy_hourday_plot.R
@@ -28,3 +28,30 @@ test_that("Arrivals_Occupancy_January-March 2015",{
 
 
 })
+
+test_that("Arrivals_Occupancy_test_2",{
+
+  load("testdata/arrivals_occupancy_test_2.rda")
+
+  #Specify correct results
+  correct_answers_arriv_occ <- tibble::tibble(
+    Hour = as.character(c(0, 1, 2, 3, 4, 5, 6, 7, 8,    9,   10, 11,   12,     13,   14,    15,    16, 17, 18,    19,    20,    21,    22,   23)),
+
+    Average_arrivals = as.numeric(c(0, 0, 0, 0, 0, 0, 0, 0, 0.5, 0.5, 0.5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)),
+    Average_occ = as.numeric(c(0.5, 0.5, 0.5, 0.5, 0.5, 0.5,  0.5, 0.5, 0, 0.5, 1, 1, 1, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5,  0.5, 0.5, 0.5,  0.5, 0.5)))
+
+
+  #Run Admission Discharges graph
+
+  result_arriv_occ_jan_march <- arrival_occupancy(start_date = as.Date("2018-12-10",tz = "Europe/London"), end_date = as.Date("2018-12-11",tz = "Europe/London"),
+                                                  data = arrivals_occupancy_test_2, plot_chart = TRUE, hospital_name = "Chelsea & Westminster")
+
+  result_ariv_occ_data_jm <- result_arriv_occ_jan_march$data
+  result_ariv_occ_data_jm$Hour <- as.character(result_ariv_occ_data_jm$Hour)
+
+
+  #Test results are correct
+  expect_equal(as.data.frame(correct_answers_arriv_occ), as.data.frame(result_ariv_occ_data_jm), tolerance = 0.01)
+
+
+})
