6a94e8b2f32fdbe614628b4e245af1d91289f780
Burcea <gburcea@ic.ac.uk>
2019-01-02 16:39:32 +0000

    re-thought the code since discovered a library pad in which padr function adds a new column with the date time period in question, therefore no need for left join. Amazing libary.

--- R/arrival_occupancy.R
+++ R/arrival_occupancy.R
@@ -15,90 +15,46 @@
 #' @export
 #'
 #' @examples
-arrival_occupancy <- function(start_date = as.Date("2012-01-01", tz = "Europe/London"),
-                              end_date = as.Date("2015-01-01", tz = "Europe/London"),
-                              data, plot_chart, hospital_name = "Chelsea & Westminster"){
+####################################################################################################################
+###3.A&E Arrivals and Occupancy, 3rd of March to 27th of April, 2015 ###############################################
+####################################################################################################################
+arrival_occupancy <- function(start_date = as.Date("2012-01-01", tz = "Europe/London"),        ############
+                                       end_date = as.Date("2015-01-01", tz = "Europe/London"),          ############
+                                       data, plot_chart, hospital_name = "Chelsea & Westminster"){
 
 
-  # extracting Month and hour from Time column and
-  # count = occupancy.
-  # ASK TOM FOR THE AVERAGES - WE GET DIFFERENT AVERAGES DEPENDENT OF THE GROUP BY IN DIFFERENT CASES - GROUP_BY WITH DAY AND HOUR GIVES DIFFERENT AVERAGES
+
+    dt_date <-  data %>%
+    dplyr::filter(Admissions > start_date & Discharges < end_date) %>%
+    dplyr::select(IDcol, Admissions, Discharges)
+
   # using gather function to create a new column with date; and filter only by Emergency Department
-  #arrivals_occupancy_jan_march <-  data %>%
-    #tidyr::gather(key = Type, Time, Admissions:Discharges) %>%
-    #dplyr::filter(PatientType == "Emergency" & EpisodeNumber == 1)
+  arrivals_occupancy_jan_march <-  dt_date %>%
+    tidyr::gather(key = Type, Time, Admissions:Discharges) %>%
+    dplyr::mutate(Change = dplyr::if_else(Type == "Admissions", 1, -1)) %>%
+    dplyr::group_by(time_hr = lubridate::floor_date(Time, "1 hour")) %>%
+    dplyr::summarise(Arrivals = sum(Type == "Admissions"),
+              Change = sum(Change)) %>%
+    padr::pad(start_val = start_date, end_val = end_date) %>%
+    tidyr::replace_na(list(Arrivals = 0, Change = 0)) %>%
+    dplyr::mutate(Occupancy = cumsum(Change))
 
 
-  # extracting year, month, day and hour from Time column and count = occupancy
-  dt_sep_occ <- data %>%
-    dplyr::mutate(
-      Time_rounded = lubridate::ymd_hms(Time_rounded),
-      Hour = lubridate::hour(Time_rounded),
-      Time = lubridate::date(Time_rounded)) %>%
-    dplyr::group_by(Time, Hour) %>%
-    dplyr::tally() %>%
-    dplyr::rename(Occupancy = n)
-
-
-  #This format is more flexible than the one I suggested
-  df_period <- seq.POSIXt(from = as.POSIXct(start_date, tz = "Europe/London"),
-                          to = as.POSIXct(end_date, tz= "Europe/London"),
-                          by = "hour")
-
-  #df_period
-  #Place the new list, with dates, into a tibble and format it accordingly.
-  #DO the same as with the adm column by finding the weekday, and merged with the admission table
-
-  df_adm_date <- tibble::as.tibble(df_period)
-
-  df_date_adm_ren <- df_adm_date %>%
-    dplyr::rename(Time_rounded = "value") %>%
-    dplyr::mutate(Hour = lubridate::hour(Time_rounded),
-                  Time = lubridate::date(Time_rounded)) %>%
-    dplyr::select(Time, Hour)
-
-
-  table_average_occ <- dplyr::left_join(df_date_adm_ren, dt_sep_occ, by = c("Time", "Hour"))
-
-  dt_occ_final <- table_average_occ %>%
-    tidyr::replace_na(list(Occupancy  = 0))
-
-  tbl_avg_occ <- dt_occ_final %>%
+  avg_arriv_occup_jan_march <- arrivals_occupancy_jan_march %>%
+    dplyr::mutate(Hour = lubridate::hour(time_hr)) %>%
     dplyr::group_by(Hour) %>%
-    dplyr::summarize(Average_occ = round(mean(Occupancy), digits = 2))
-
-  # take out the start-datetime only -> this will be used later to be added to a plot alongside the occupancy
-  dt_sep_arrivals <- data  %>%
-    dplyr::filter(Type == "Admissions") %>%
-    dplyr::select(IDcol, Time_rounded) %>%
-    dplyr::mutate(
-      Time = lubridate::date(Time_rounded),
-      Day = lubridate::day(Time_rounded),
-      Hour = lubridate::hour(Time_rounded)) %>%
-    dplyr::group_by(Time, Hour) %>%
-    dplyr::tally() %>%
-    dplyr::rename(Arrivals = n)
-
-  table_average_arrivals <- dplyr::left_join(df_date_adm_ren, dt_sep_arrivals, by = c("Time", "Hour"))
-
-  dt_avg_arrivals <- table_average_arrivals %>%
-    tidyr::replace_na(list(Arrivals  = 0))
-
-  tbl_avg_arrivals <- dt_avg_arrivals %>%
-    dplyr::group_by(Hour) %>%
-    dplyr::summarize(Average_arrivals = round(mean(Arrivals), digits = 2))
-
-  tbl_occ_arriv <- dplyr::left_join(tbl_avg_arrivals, tbl_avg_occ, by= c("Hour"))
+    dplyr::summarize(Average_arrivals = mean(Arrivals),
+              Average_occupancy = mean(Occupancy))
 
 
   # Set the title
   title_stub <- " hospital: Hourly A&E occupancy and arrival profile, "
-  hospital_name <- "Chelsea & Westminster"
+  #hospital_name <- "Chelsea & Westminster"
   start_date_title <- format(as.Date(start_date), format = "%d %B %Y")
   end_date_title <- format(as.Date(end_date), format = "%d %B %Y")
   chart_title <- paste0(hospital_name, title_stub, start_date_title, " to ", end_date_title)
 
-  plt_occ_percent <- ggplot2::ggplot(tbl_occ_arriv, ggplot2::aes(x = as.numeric(Hour), y = Average_occ, group = Hour)) +
+  plt_occ_percent <- ggplot2::ggplot(data = avg_arriv_occup_jan_march, ggplot2::aes(x = as.numeric(Hour), y = Average_occupancy, group = Hour))+
     ggplot2::geom_bar(stat = "identity", alpha=0.7, width = 0.40, ggplot2::aes(fill = "Occupancy")) +
     ggplot2::scale_x_continuous(breaks = 0:23, expand = c(0, 0.2)) + #, expand = c(0, 0)
     #ggplot2::scale_y_continuous(expand = c(0, 0)) + # breaks = c(0, 5, 10, 15, 20, 25))
@@ -117,15 +73,18 @@ arrival_occupancy <- function(start_date = as.Date("2012-01-01", tz = "Europe/Lo
 
   plt_occ_percent
 
+  #################################################################################################################
+
+
   if(plot_chart == TRUE){
 
     plt_occ_percent
 
   }else{
 
-    plt_occ_percent$data %>% select(Hour, Average_arrivals, Average_occ)
+    plt_occ_percent$data %>% select(Hour, Average_arrivals , Average_occupancy)
 
   }
 
-}
-
+}  ############
+####################################################################################################################
--- tests/testthat/test_arrival_occupancy_hourday_plot.R
+++ tests/testthat/test_arrival_occupancy_hourday_plot.R
@@ -2,49 +2,37 @@ context("Occupancy and Admissions by hour of the day")
 library(hospitalflow)
 
 
-test_that("Arrivals_Occupancy_January-March 2015",{
-
-  load("testdata/arrivals_occupancy_jan_march.rda")
-
-  #Specify correct results
-  correct_answers_arriv_occ <- tibble::tibble(
-           Hour = as.character(c(0, 1, 2, 3, 4, 5, 6, 7, 8,    9,   10, 11,   12,     13,   14,    15,    16, 17, 18,    19,    20,    21,    22,   23)),
-
-    Average_arrivals = as.numeric(c(3.62, 2.66, 2.03, 1.72, 1.55, 1.41, 1.32, 1.27, 2.35, 4.24, 6.98, 7.66,  7.54,   6.8,   7.12,  6.63, 6.9,  6.46,  6.64,  6.55,  6.04,  5.25,  4.55, 3.55)),
-         Average_occ = as.numeric(c(9.14, 7.12, 6.12, 5.14, 4.41, 3.76, 3.18, 3.11, 3.48, 5.82, 9.01, 10.87, 12.45, 12.45, 13.76, 13.58, 13.45, 13.73, 13.21, 13.21, 12.53, 11.51, 11.37, 9.36)))
-
-
-  #Run Admission Discharges graph
-
-  result_arriv_occ_jan_march <- arrival_occupancy(start_date = as.Date("2015-01-01",tz = "Europe/London"), end_date = as.Date("2015-03-31",tz = "Europe/London"),
-                                                  data = arrivals_occupancy_jan_march, plot_chart = TRUE, hospital_name = "Chelsea & Westminster")
-
-  result_ariv_occ_data_jm <- result_arriv_occ_jan_march$data
-  result_ariv_occ_data_jm$Hour <- as.character(result_ariv_occ_data_jm$Hour)
-
-
-  #Test results are correct
-  expect_equal(as.data.frame(correct_answers_arriv_occ), as.data.frame(result_ariv_occ_data_jm), tolerance = 0.01)
-
-
-})
-
-test_that("Arrivals_Occupancy_test_2",{
+test_that("Arrivals_Occupancy_test",{
 
   load("testdata/arrivals_occupancy_test_2.rda")
 
   #Specify correct results
   correct_answers_arriv_occ <- tibble::tibble(
-    Hour = as.character(c(0, 1, 2, 3, 4, 5, 6, 7, 8,    9,   10, 11,   12,     13,   14,    15,    16, 17, 18,    19,    20,    21,    22,   23)),
+    Hour = as.character(c(0, 1, 2, 3, 4, 5, 6, 7, 8,  9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22,  23)),
 
     Average_arrivals = as.numeric(c(0, 0, 0, 0, 0, 0, 0, 0, 0.5, 0.5, 0.5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)),
-    Average_occ = as.numeric(c(0.5, 0.5, 0.5, 0.5, 0.5, 0.5,  0.5, 0.5, 0, 0.5, 1, 1, 1, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5,  0.5, 0.5, 0.5,  0.5, 0.5)))
+    Average_occupancy = as.numeric(c(0.5, 0.5, 0.5, 0.5, 0.5, 0.5,  0.5, 0.0, 0.5, 0.5, 1, 1, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5,  0.5, 0.5, 0.5,  0.5, 0.5)))
+
+
+  #creating a dataset
+
+
+  dt1 <- c("2018-12-10 09:30:00","2018-12-10 10:15:00",  "2018-12-11 08:05:00")
+
+  dt2 <- c("2018-12-10 12:30:00", "2018-12-11 07:45:00", "2018-12-11 09:05-00")
+
+  Admissions <- as.POSIXct(dt1, tz = "Europe/London")
+  Discharges <- as.POSIXct(dt2,tz = "Europe/London")
+
+  test_arrivals_occupancy <- tibble::tibble(IDcol = 101:103,
+                                            Admissions,
+                                            Discharges)
 
 
   #Run Admission Discharges graph
 
-  result_arriv_occ_jan_march <- arrival_occupancy(start_date = as.Date("2018-12-10",tz = "Europe/London"), end_date = as.Date("2018-12-11",tz = "Europe/London"),
-                                                  data = arrivals_occupancy_test_2, plot_chart = TRUE, hospital_name = "Chelsea & Westminster")
+  result_arriv_occ_jan_march <- arrival_occupancy(start_date = as.POSIXct("2018-12-10 00:00",tz = "Europe/London"), end_date = as.POSIXct("2018-12-11 23:00",tz = "Europe/London"),
+                                                  data = test_arrivals_occupancy, plot_chart = TRUE, hospital_name = "Chelsea & Westminster")
 
   result_ariv_occ_data_jm <- result_arriv_occ_jan_march$data
   result_ariv_occ_data_jm$Hour <- as.character(result_ariv_occ_data_jm$Hour)
