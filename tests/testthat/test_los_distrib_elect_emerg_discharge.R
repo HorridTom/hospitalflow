2f21ced98871ac052ed237b72f174d6a90106920
Burcea <gburcea@ic.ac.uk>
2019-01-07 16:58:12 +0000

    came up with an improvised dataset in order to test the function.

--- tests/testthat/test_los_distrib_elect_emerg_discharge.R
+++ tests/testthat/test_los_distrib_elect_emerg_discharge.R
@@ -2,54 +2,77 @@ context("Los distribution Elective and Emergency plot")
 library(hospitalflow)
 
 
-test_that("Los distribution Elective and Emergency plot for January-March 2015",{
 
-  load("testdata/test_data_los_bin_emerg_elect.rda")
+test_that("Los distribution Elective and Emergency for improvised data",{
+
+  #load("testdata/test_data_los_bin_emerg_elect.rda")
 
   #Specify correct results
   correct_answers <- tibble::tibble(
 
-    Spell_type = as.character(c("Elective", "Elective", "Elective", "Elective", "Elective", "Elective", "Elective", "Elective", "Elective", "Elective", "Elective",
-                             "Elective", "Elective", "Elective", "Elective",  "Elective", "Elective", "Elective", "Elective", "Elective", "Elective","Elective", "Elective",
-                             "Emergency", "Emergency", "Emergency", "Emergency", "Emergency", "Emergency", "Emergency", "Emergency",
-                             "Emergency", "Emergency", "Emergency", "Emergency", "Emergency", "Emergency", "Emergency", "Emergency",
-                             "Emergency", "Emergency", "Emergency", "Emergency", "Emergency", "Emergency", "Emergency")),
+    Spell_type = as.character(c("Elective", "Elective", "Elective", "Emergency", "Emergency", "Emergency")),
 
-    losbinned =  forcats::fct_relevel(c( "8hrs", "16hrs", "1 d",  "1 d 8hrs", "1 d 16hrs",
-                                               "2 d",  "2 d 8hrs", "2 d 16hrs",
-                                               "3 d",  "3 d 8hrs", "3 d 16hrs",
-                                               "4 d",  "4 d 8hrs", "4 d 16hrs",
-                                               "5 d",  "5 d 8hrs", "5 d 16hrs",
-                                                       "6 d 8hrs", "6 d 16hrs",
-                              "7 - 14 d", "14 - 21 d", "21 - 28 d", "> 28 d",
-                          "0hrs", "8hrs", "16hrs", "1 d", "1 d 8hrs", "1 d 16hrs",
-                                                   "2 d", "2 d 8hrs", "2 d 16hrs",
-                                                   "3 d",              "3 d 16hrs",
-                                                   "4 d", "4 d 8hrs",  "4 d 16hrs",
-                                                   "5 d", "5 d 8hrs",  "5 d 16hrs",
-                                                   "6 d", "6 d 8hrs",
-                             "7 - 14 d", "14 - 21 d", "21 - 28 d", "> 28 d")),
+    losbinned =  as.factor(c("2 d", "3 d", "> 28 d", "2 d", "3 d", "> 28 d")),
+
+    Count = as.numeric(c(1,  1,  1, 1, 1, 2))
+  )
+
+  correct_answers <- correct_answers %>%
+    dplyr::mutate(losbinned = factor(losbinned, levels = c("2 d", "3 d", "> 28 d")))
 
 
-    Count = as.numeric(c(13, 36, 64, 104, 13, 18, 38, 5, 12, 28, 3, 6, 11, 4, 2, 4, 1, 14, 3, 40, 11, 3, 3, 1, 8, 22, 17, 7, 9, 5, 4, 4, 4, 9, 6, 3, 5, 2, 1, 2, 1, 2, 12, 2, 4, 4))
-    )
+  correct_answers$losbinned <- forcats::fct_relevel(correct_answers$losbinned)
 
-  str(correct_answers)
+  # improvise a a fake data
+
+  los1 <- c("2018-10-10 09:30:00", "2018-10-10 09:30:00", "2018-10-10 09:30:00", "2018-10-10 08:00:00", "2018-10-10 08:00:00", "2018-10-10 08:00:00", "2018-10-10 08:00:00", "2018-10-10 08:00:00")
+
+  los2 <- c("2018-12-10 09:30:00", "2018-12-10 09:30:00","2018-12-10 09:30:00" , "2018-10-12 08:00:00", "2018-10-12 08:00:00", "2018-10-13 08:00:00", "2018-10-13 08:00:00", "2018-10-10 09:00:00")
+
+
+  Admission_type <- c("Elective", "Emergency", "Emergency", "Elective", "Emergency", "Elective", "Emergency", "Emergency")
+
+  Spell_type <- c("Elective", "Emergency", "Emergency", "Elective", "Emergency", "Elective", "Emergency", "Emergency")
+
+  Ward_code <- c("DSN", "DSN", "DNA", "NAS", "BAS", "BAS","BAS","BAS")
+
+
+  Episode_number <- c(1, 1, 1, 1, 1, 1, 1, 2)
+
+  Admissions <- as.POSIXct(los1, tz = "Europe/London")
+  Discharges <- as.POSIXct(los2,tz = "Europe/London")
+
+  test_los <- tibble::tibble(IDcol = 101:108,
+                             Admissions,
+                             Discharges,
+                             Admission_type,
+                             Episode_number, Spell_type, Ward_code)
 
 
 
 
   #Run Admission Discharges graph
-  result <- los_distrib_elect_emerg_discharge(start_date = "2015-01-01 00:00:00", end_date = "2015-04-01 00:00:00", data = test_data_los_bin_emerg_elect, plot_chart = TRUE)
+  result <- los_distrib_elect_emerg_discharge(start_date = as.Date("2018-10-10",tz = "Europe/London"), end_date = as.Date("2018-12-14", tz = "Europe/London"), data = test_los,
+                                              plot_chart = TRUE, hospital_name = "Chelsea & Westminster")
 
   result_data <- result$data
   result_data$Count <- as.numeric(result_data$Count)
   result_data$Spell_type <- as.character(result_data$Spell_type)
 
+  result_data <- result_data %>%
+    dplyr::mutate(losbinned = factor(losbinned, levels = c("2 d", "3 d", "> 28 d")))
+
+
+  result_data$losbinned <- forcats::fct_relevel(result_data$losbinned)
+
+
   str(result_data)
 
   #Test results are correct
-  expect_equal(as.data.frame(result_data), as.data.frame(correct_answers))
+  expect_equal(as.data.frame(correct_answers), as.data.frame(result_data))
 
 
 })
+
+
+
