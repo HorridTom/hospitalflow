271d9b96f117de442f6814bf95ec86de59b895cf
Burcea <gburcea@ic.ac.uk>
2019-05-21 12:54:12 +0100

    creating test for los_distrib.

--- tests/testthat/test_los_distrib_method_admission.R
+++ tests/testthat/test_los_distrib_method_admission.R
@@ -6,7 +6,7 @@ test_that("Length of stay for admitted patients, for improvised data",{
   #Specify correct results
   correct_answers <- tibble::tibble(
 
-    losbinned =  as.factor(c("1d","1d","1d", "1d", "2d", "2d", "2d", "2d")),
+    losbinned =  as.factor(c("1 d","1 d","1 d", "1 d", "2 d", "2 d", "2 d", "2 d")),
 
     admission_method_type = as.factor(c( "Emergency Admissions", "Elective Admissions","Maternity Admissions" , "Other Admissions",  "Emergency Admissions", "Elective Admissions","Maternity Admissions" , "Other Admissions")),
 
@@ -14,6 +14,16 @@ test_that("Length of stay for admitted patients, for improvised data",{
     Count = as.numeric(c(2, 1, 1, 1, 2, 1, 1, 1))
   )
 
+
+  correct_answers <- correct_answers %>%
+    dplyr::mutate(losbinned =  factor(losbinned, levels = c("0hrs", "8hrs", "16hrs",
+                                                            "1 d", "1 d 8hrs", "1 d 16hrs",
+                                                            "2 d", "2 d 8hrs", "2 d 16hrs",
+                                                            "3 d", "3 d 8hrs", "3 d 16hrs",
+                                                            "4 d", "4 d 8hrs", "4 d 16hrs",
+                                                            "5 d", "5 d 8hrs", "5 d 16hrs",
+                                                            "6 d",  "6 d 8hrs","6 d 16hrs",
+                                                            "7 - 14 d", "15 - 21 d", "22 - 28 d", "> 28 d")))
 
   spell_start <- c("2019-01-01 09:00:00",  "2019-01-02 16:00:00",   "2019-01-03 16:00:00", "2019-01-05 17:00:00", "2019-01-02 01:00:00",  "2019-01-05 16:00:00", "2019-01-02 01:00:00", "2019-01-05 16:00:00" ,"2019-01-02 01:00:00", "2019-01-05 16:00:00")
 
@@ -32,7 +42,6 @@ test_that("Length of stay for admitted patients, for improvised data",{
 
 
 
-
   #Run Admission Discharges graph
   result <- hospitalflow::los_distrib_method_admission(start_date = "2019-01-01 00:00:00", end_date = "2019-01-07 00:00:00", data = test_los_admission, plot_chart = FALSE, hospital_name = "Queh")
 
@@ -41,6 +50,15 @@ test_that("Length of stay for admitted patients, for improvised data",{
 
   result_data <- result %>% dplyr::select(losbinned, admission_method_type, Count)
 
+  result_data <- result_data %>%
+    dplyr::mutate(losbinned =  factor(losbinned, levels = c("0hrs", "8hrs", "16hrs",
+                                                            "1 d", "1 d 8hrs", "1 d 16hrs",
+                                                            "2 d", "2 d 8hrs", "2 d 16hrs",
+                                                            "3 d", "3 d 8hrs", "3 d 16hrs",
+                                                            "4 d", "4 d 8hrs", "4 d 16hrs",
+                                                            "5 d", "5 d 8hrs", "5 d 16hrs",
+                                                            "6 d",  "6 d 8hrs","6 d 16hrs",
+                                                            "7 - 14 d", "15 - 21 d", "22 - 28 d", "> 28 d")))
 
   #Test results are correct
   expect_equal(result_data, correct_answers, tolerance = 0.1)
