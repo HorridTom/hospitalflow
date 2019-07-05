b558e68252bad2e8c242f74151c3b312332776ea
Burcea <gburcea@ic.ac.uk>
2019-07-05 16:11:58 +0100

    replaced the previous test on make_spell_table since the previous test did not contain admission_type, thus getting errors. Side note: admission method was obtain at the point when we got the function on length of stay for method type admissions.

--- tests/testthat/test_make_spell.R
+++ tests/testthat/test_make_spell.R
@@ -1,6 +1,7 @@
 context("Make a spell table function")
 library(tidyverse)
 
+
 test_that("spell table is created correctly",{
 
   correct_answers <- tibble::tibble(
@@ -8,10 +9,10 @@ test_that("spell table is created correctly",{
     spell_number = as.character(c("1", "2", "3", "4", "5", "6", "7", "8", "9")),
     spell_start = as.POSIXct(c("2019-01-02 17:00", "2019-01-03 08:00", "2019-01-03 19:00", "2019-01-01 17:00", "2019-01-02 08:00", "2019-01-03 08:00", "2019-01-04 14:00", "2019-01-05 06:00", "2019-01-04 14:00")),
     spell_end = as.POSIXct(c("2019-01-02 21:30", "2019-01-03 10:00", "2019-01-03 21:00", "2019-01-02 00:00", "2019-01-02 10:00", "2019-01-03 11:00", "2019-01-05 03:00", "2019-01-05 13:00", "2019-01-04 17:00")),
-    number_of_episodes = as.character(c("2", "1", "1", "1", "1", "1", "2", "1", "1"))
+    number_of_episodes = as.character(c("2", "1", "1", "1", "1", "1", "2", "1", "1")),
+    admission_method_type = as.character(c("Emergency Admissions", "Emergency Admissions", "Emergency Admissions", NA, "Elective Admissions", "Elective Admissions", "Maternity Admissions", "Maternity Admissions", NA))
 
   )
-
 
   pseudo_id <- as.character(c("112", "113", "114", "115"))
 
@@ -29,11 +30,14 @@ test_that("spell table is created correctly",{
 
   age_band_start <- c("1-4 yrs", "5-9", "10-14", "95+")
 
+
+
   ed_data_age_sex <- tibble::tibble(pseudo_id, episode_id,
-                            start_datetime,
-                            end_datetime,
-                            gender,
-                            age_band_start)
+                                    start_datetime,
+                                    end_datetime,
+                                    gender,
+                                    age_band_start
+  )
 
 
   pseudo_id <- as.character(c("112", "112", "112", "113", "113", "114", "114"))
@@ -51,15 +55,18 @@ test_that("spell table is created correctly",{
 
   age_band_start <- c("1-4 yrs", "1-4 yrs", "5-9", "5-9","5-9","10-14","10-14")
 
+  admission_method <- c("Accident and emergency", "Accident and emergency", "Accident and emergency", "Waiting list","Waiting list", "Admitted ante-partum", "Admitted ante-partum")
+
   inpatient_data_age_sex <- tibble::tibble(pseudo_id, episode_id,
-                                   start_datetime,
-                                   end_datetime,
-                                   gender,
-                                   age_band_start)
+                                           start_datetime,
+                                           end_datetime,
+                                           gender,
+                                           age_band_start,
+                                           admission_method)
 
 
-  result <- make_spell_table(ed_data_age_sex, inpatient_data_age_sex, same_type_episode_lag = 1, different_type_episode_lag = 6) %>%
-    dplyr::select(spell_number, spell_start, spell_end, number_of_episodes)
+  result <- hospitalflow::make_spell_table(ed_data_age_sex, inpatient_data_age_sex, same_type_episode_lag = 1, different_type_episode_lag = 6) %>%
+    dplyr::select(spell_number, spell_start, spell_end, number_of_episodes, admission_method_type)
 
   result$spell_number <- as.character(result$spell_number)
   result$number_of_episodes <- as.character(result$number_of_episodes)
@@ -69,3 +76,6 @@ test_that("spell table is created correctly",{
 
 
 })
+
+
+
