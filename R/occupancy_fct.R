0b7f102ada4ecdb0756236b6097c452b3344e571
Burcea <gburcea@ic.ac.uk>
2019-08-02 16:55:08 +0100

    added a new branch with a new function - occupancy_fct. It calculates the occupancy between two date periods, at each hour.

---
+++ R/occupancy_fct.R
@@ -0,0 +1,26 @@
+#' occupancy_fct
+#'
+#' @param start_date
+#' @param end_date
+#' @param data
+#'
+#' @return
+#' @export
+#'
+#' @examples
+occupancy_fct <- function(start_date = as.Date("2016-06-01", tz = "Europe/London"),
+                          end_date = as.Date("2016-06-08", tz = "Europe/London"),
+                          data){
+
+  # using gather function to create a new column with date
+  occupancy <-  data %>%
+    tidyr::gather(key = type, time, spell_start:spell_end) %>%
+    dplyr::mutate(change = dplyr::if_else(type == "spell_start", 1, -1)) %>%
+    dplyr::group_by(time_hr = lubridate::floor_date(time, "1 hour")) %>%
+    dplyr::summarise(change = sum(change)) %>%
+    padr::pad(start_val = start_date, end_val = end_date) %>%
+    tidyr::replace_na(list(change = 0)) %>%
+    dplyr::mutate(occupancy = cumsum(change)) %>%
+    tidyr::drop_na()
+
+}
