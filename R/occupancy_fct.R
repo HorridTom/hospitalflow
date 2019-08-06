889ddffee2ba6a1692c64b9afa140f7693ebc7cd
Burcea <gburcea@ic.ac.uk>
2019-08-06 11:52:16 +0100

    polished function - added dates accordingly to what function should do, that of occupancy per hour. And second, added new test with real data, of 1 row only. test is passing.

--- R/occupancy_fct.R
+++ R/occupancy_fct.R
@@ -12,11 +12,8 @@ occupancy_fct <- function(start_date = as.POSIXct("2016-06-01 00:00:00", tz = "E
                           end_date = as.POSIXct("2016-06-08 00:00:00", tz = "Europe/London"),
                           data){
 
-  start_date = as.POSIXct("2016-06-10 09:00:00", tz = "Europe/London")
-  end_date = as.POSIXct("2016-06-10 10:00:00", tz = "Europe/London")
-
   # using gather function to create a new column with date
-  occupancy <-  test_arrivals_occupancy %>%
+  occupancy <-  data %>%
     tidyr::gather(key = type, time, spell_start:spell_end) %>%
     dplyr::mutate(change = dplyr::if_else(type == "spell_start", 1, -1)) %>%
     dplyr::group_by(time_hr = lubridate::floor_date(time, "1 hour")) %>%
@@ -24,7 +21,7 @@ occupancy_fct <- function(start_date = as.POSIXct("2016-06-01 00:00:00", tz = "E
     padr::pad(start_val = start_date, end_val = end_date) %>%
     tidyr::replace_na(list(change = 0)) %>%
     dplyr::mutate(occupancy = cumsum(change)) %>%
-    dplyr::select(occupancy) %>%
+    dplyr::select(-change) %>%
     tidyr::drop_na()
 
 }
--- tests/testthat/test_occupancy_hr.R
+++ tests/testthat/test_occupancy_hr.R
@@ -1,7 +1,7 @@
 context("Occupancy by hour of a date")
 library(hospitalflow)
 
-test_that("Occupancy by hour of a date is generated correctly for two dates",{
+test_that("Occupancy by hour of a date is generated correctly for two dates for improvised data",{
 
   #Specify correct results
   correct_answers <- tibble::tibble(
@@ -26,6 +26,30 @@ test_that("Occupancy by hour of a date is generated correctly for two dates",{
   result_occ <- hospitalflow::occupancy_fct(start_date = as.POSIXct("2018-12-10 09:00:00",tz = "Europe/London"),
                                             end_date = as.POSIXct("2018-12-10 09:00:00",tz = "Europe/London"),
                                             data = test_arrivals_occupancy)
+
+  #Test results are correct
+  expect_equal(as.data.frame(correct_answers), as.data.frame(result_occ), tolerance = 0.01)
+
+})
+
+
+
+test_that("Occupancy by hour of a date is generated correctly, for real data",{
+
+  load("D:/Rprojects/hospitalflow/tests/testthat/testdata/occupancy/occupancy_test.rda")
+
+  occupancy_test <- occupancy %>%
+    dplyr::select(spell_number, spell_start, spell_end)
+
+  #Specify correct results
+  correct_answers <- tibble::tibble(
+    time_hr = as.POSIXct(c("2016-06-28 18:00:00", "2016-06-28 19:00:00")),
+
+    occupancy = as.numeric(c(1, 1)))
+
+  result_occ <- hospitalflow::occupancy_fct(start_date = as.POSIXct("2016-06-28 18:00:00",tz = "Europe/London"),
+                                            end_date = as.POSIXct("2016-06-28 19:00:00",tz = "Europe/London"),
+                                            data = occupancy_test)
 
   #Test results are correct
   expect_equal(as.data.frame(correct_answers), as.data.frame(result_occ), tolerance = 0.01)
