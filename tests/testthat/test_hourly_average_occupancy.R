4f41d39074c2df83806b5a5c5ac354b836505aa0
Burcea <gburcea@ic.ac.uk>
2019-08-06 13:49:31 +0100

    added a new column needed for analysis - see line 57.

--- tests/testthat/test_hourly_average_occupancy.R
+++ tests/testthat/test_hourly_average_occupancy.R
@@ -46,3 +46,28 @@ test_that("arrivals and occupancy by hour of the day is correctly calculated",{
 })
 
 
+context("Arrivals and Occupancy by hour of the day")
+library(hospitalflow)
+
+test_that("arrivals and occupancy by hour of the day is correctly calculated",{
+
+  load("D:/Rprojects/hospitalflow/tests/testthat/testdata/occupancy/occupancy_test.rda")
+
+  occupancy_test <- occupancy %>%
+    dplyr::select(spell_number, spell_start, spell_end)
+
+  #Specify correct results
+  correct_answers <- tibble::tibble(
+    time_hr = as.POSIXct(c("2016-06-28 18:00:00", "2016-06-28 19:00:00")),
+
+    occupancy = as.numeric(c(1, 1)))
+
+  result_occ <- ae_arrival_occupancy_fct(start_date = as.POSIXct("2016-06-28 18:00:00",tz = "Europe/London"),
+                                            end_date = as.POSIXct("2016-06-28 19:00:00",tz = "Europe/London"),
+                                            data = occupancy_test)
+
+  #Test results are correct
+  expect_equal(as.data.frame(correct_answers), as.data.frame(result_occ), tolerance = 0.01)
+
+})
+
