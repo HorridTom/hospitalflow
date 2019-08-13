a88d8ea6ec21cf91fb8f14d80865f04a44b7124b
Imogen Connor-Helleur <imooconnor@outlook.com>
2019-08-13 14:49:41 +0100

    Made function faster and added some missing package calls.

--- R/ed_occupancy_on_arrival.R
+++ R/ed_occupancy_on_arrival.R
@@ -13,7 +13,8 @@ ed_occupancy_on_arrival <- function(df){
   df <- df %>%
     dplyr::rowwise() %>%
     dplyr::mutate(occupancy_on_arrival = hospitalflow::occupancy(start_datetime, df,
-                                                                 start_time = "start_datetime", end_time = "end_datetime"))
+                                                                 start_time = "start_datetime",
+                                                                 end_time = "end_datetime"))
 
   df
 
@@ -36,11 +37,11 @@ ed_occupancy_on_arrival <- function(df){
 #' @examples
 wait_times_over_4hrs_Vs_occupancy <- function(df, startDate, endDate, hospitalName, plotChart = T){
 
-  df <- filter(df, start_datetime >= startDate, end_datetime <= endDate)
+  df <- dplyr::filter(df, start_datetime <= endDate, end_datetime >= startDate)
 
   df <- ed_occupancy_on_arrival(df)
   df <- df %>%
-    #dplyr::filter(start_datetime >= startDate, end_datetime <= endDate) %>%
+    dplyr::filter(start_datetime >= startDate, end_datetime <= endDate) %>%
     dplyr::filter(attendance_category != "Follow up - Planned") %>%
     dplyr::mutate(LoS_hrs =
                     as.numeric(difftime(end_datetime, start_datetime, units = "hours"))) %>%
@@ -75,30 +76,30 @@ wait_times_over_4hrs_Vs_occupancy <- function(df, startDate, endDate, hospitalNa
 
   df_W4T_merged <- dplyr::inner_join(df_all, df_W4T_over_4hrs)
   df_W4T_merged <- df_W4T_merged %>%
-    dplyr::mutate(perc_W4T_over_4hrs = (n_W4T_over_4hrs*100)/n_all)
+                        dplyr::mutate(perc_W4T_over_4hrs = (n_W4T_over_4hrs*100)/n_all)
 
 
   df_to_plot <- dplyr::inner_join(df_LoS_merged, df_W4T_merged)
 
   #generate deciles from attendances profile
-  deciles <- quantile(df$occupancy_on_arrival, probs = seq(0, 1, length = 11), type = 5)
-  deciles_tib <- tibble(deciles = deciles, labels = paste(seq(0, 100, 10),"%"))
-  deciles_tib <- filter(deciles_tib, deciles > df_to_plot$occupancy_on_arrival[1], deciles < df_to_plot$occupancy_on_arrival[nrow(df_to_plot)])
+  deciles <- stats::quantile(df$occupancy_on_arrival, probs = seq(0, 1, length = 11), type = 5)
+  deciles_tib <- tibble::tibble(deciles = deciles, labels = paste(seq(0, 100, 10),"%"))
+  deciles_tib <- dplyr::filter(deciles_tib, deciles > df_to_plot$occupancy_on_arrival[1], deciles < df_to_plot$occupancy_on_arrival[nrow(df_to_plot)])
 
 
   plot <- ggplot2::ggplot(df_to_plot,
-                          aes(x = occupancy_on_arrival)) +
-    ggplot2::geom_line(aes(y=n_all, colour = "n_all")) +
-    ggplot2::geom_smooth(aes(y=perc_LoS_over_4hrs,
+                          ggplot2::aes(x = occupancy_on_arrival)) +
+    ggplot2::geom_line(ggplot2::aes(y=n_all, colour = "n_all")) +
+    ggplot2::geom_smooth(ggplot2::aes(y=perc_LoS_over_4hrs,
                              colour = "perc_LoS_over_4hrs")) +
-    ggplot2::geom_smooth(aes(y=perc_W4T_over_4hrs,
+    ggplot2::geom_smooth(ggplot2::aes(y=perc_W4T_over_4hrs,
                              colour = "per_W4T_over_4hrs")) +
-    ggplot2::geom_point(aes(y=perc_LoS_over_4hrs,
+    ggplot2::geom_point(ggplot2::aes(y=perc_LoS_over_4hrs,
                              colour = "perc_LoS_over_4hrs")) +
-    ggplot2::geom_point(aes(y=perc_W4T_over_4hrs,
+    ggplot2::geom_point(ggplot2::aes(y=perc_W4T_over_4hrs,
                              colour = "per_W4T_over_4hrs")) +
     ggplot2::theme_bw() +
-    ggplot2::theme(legend.title = element_blank(),
+    ggplot2::theme(legend.title = ggplot2::element_blank(),
                    legend.position = "bottom") +
     ggplot2::labs(title = "Occupancy on Arrival versus Percentage of Unscheduled \nED Attendances with LoS and Wait Time for Treatment over 4hrs",
                   subtitle = paste(hospitalName,"\nbetween ", strftime(startDate, "%d/%m/%Y"),
@@ -111,7 +112,7 @@ wait_times_over_4hrs_Vs_occupancy <- function(df, startDate, endDate, hospitalNa
                                   "% of unscheduled ED \nattendances with LoS over 4hrs",
                                   "% of unscheduled ED attendances \nwith wait time for treatment over 4hrs"),
                                 values = c("dodgerblue2", "firebrick3", "seagreen4")) +
-    ggplot2::guides(color = guide_legend(reverse = TRUE)) +
+    ggplot2::guides(color = ggplot2::guide_legend(reverse = TRUE)) +
     ggplot2::geom_vline(xintercept = deciles_tib$deciles,
                         linetype = "dashed",
                         colour = "azure3",
