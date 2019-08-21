80dc2804409ca0d6e7e5b65b0ccdd5fad38c552e
Burcea <gburcea@ic.ac.uk>
2019-08-21 09:51:01 +0100

    updated the function to encapsulate qicharts and ggplot2. Additionally, I have created a spc_rules file that helps the four_hours_perf function.

--- R/four_hrs_perf.R
+++ R/four_hrs_perf.R
@@ -10,7 +10,7 @@
 #' @export
 #'
 #' @examples
-four_hrs_perf <- function(start_date = as.Date("2012-01-01", tz = "Europe/London"),
+four_hrs_perf <- function(start_date = as.Date("2015-01-01", tz = "Europe/London"),
                           end_date = as.Date("2015-01-01", tz = "Europe/London"),
                           data, plot_chart, hospital_name = "Hospital_Name"){
 
@@ -19,9 +19,9 @@ four_hrs_perf <- function(start_date = as.Date("2012-01-01", tz = "Europe/London
     dplyr::filter(start_datetime <= end_date & end_datetime >= start_date)
 
 
-  # calculating total time by using difftime #
+  # calculating total time by using difftime ####
   # length of stay for the emergency department #
-  ######################################
+  ##########################################################
   # creating a new variable - under 4 hrs and above 4 hrs ##
 
   dt_los <-  dt_select  %>%
@@ -43,8 +43,8 @@ four_hrs_perf <- function(start_date = as.Date("2012-01-01", tz = "Europe/London
 
 
   # Set the title
-  title_stub <- " Hospital LoS distribution for admitted patients, "
-  hospital_name <- "Chelsea & Westminster"
+  title_stub <- ": Number of A&E attendances, "
+  hospital_name <- "Hospital_name"
   start_date_title <- format(as.Date(start_date), format = "%d %B %Y")
   end_date_title <- format(as.Date(end_date), format = "%d %B %Y")
   chart_title <- paste0(hospital_name, title_stub, start_date_title, " to ", end_date_title)
@@ -53,48 +53,67 @@ four_hrs_perf <- function(start_date = as.Date("2012-01-01", tz = "Europe/London
   # function to plot the 4 hrs emergency performance
   # Plot all days - see Tom's AE APP
 
-  plot <- qicharts2::qic(Time, under_4hrs,
-                         n = N,
-                         data = sum_4hrs_perf,
-                         chart = 'p',
-                         ylab = "percent",
-                         show.grid = TRUE,
-                         #freeze = 1,
-                         #part = 11,
-                         #multiply= 100,
-                         x.format = "%d-%m-%Y",
-                         x.angle = 45,
-                         xlab = " Compliance with 4hr emergency care standard",
-                         title = chart_title)
+  pct <- qicharts2::qic(Time, under_4hrs, n = N, data = sum_4hrs_perf, chart = 'p', ylab = "percent",
+                        show.grid = TRUE, multiply= 100)
+
+  pct$data$x <- as.Date(pct$data$x, tz = "Europe/London")
+  cht_data <- add_rule_breaks(pct$data)
+  pct <- ggplot2::ggplot(cht_data, ggplot2::aes(x, y, label = x))
+  cutoff <- data.frame(yintercept= 95, cutoff=factor(95))
+
+  #convert arguments to dates and round to nearest quarter
+  st.dt <- as.Date(start_date, format = "%Y-%m-%d", tz = "Europe/London")
+  ed.dt <- as.Date(end_date, format = "%Y-%m-%d", tz = "Europe/London")
+  #q.st.dt <- as.Date(zoo::as.yearqtr(st.dt, format = "%Y-%m-%d"))
+  #q.ed.dt <- as.Date(zoo::as.yearqtr(ed.dt, format = "%Y-%m-%d"), frac = 1) + 1
+  cht_axis_breaks <- seq(st.dt, ed.dt, by = "quarters")
+  ylimlow <- min(min(pct$data$y, na.rm = TRUE),min(pct$data$lcl, na.rm = TRUE))
+
+  four_hr_plot <- format_control_chart(pct, r1_col = "orange", r2_col = "steelblue") +
+    ggplot2::geom_hline(ggplot2::aes(yintercept = yintercept, linetype = cutoff),
+                        data = cutoff, colour = "#00BB00", linetype = 1) +
+    ggplot2::scale_x_date(date_breaks = "1 month", labels = scales::date_format("%Y-%m-%d"),
+                          breaks = cht_axis_breaks) + #limits = c(st.dt, ed.dt)
+    ggplot2::annotate("text", ed.dt - 90, 95, vjust = -2, label = "95% Target", colour = "#00BB00") +
+    ggplot2::ggtitle(chart_title) +
+    ggplot2::theme(plot.title = ggplot2::element_text(size = 11, face = "bold")) +
+    #ggplot2::theme(axis.text.x = ggplot2::element_text(angle = 45, size = 10))
+    ggplot2::labs(x = "Month", y = "Percentage within 4 hours",
+         caption = "*Shewart chart rules apply (see Understanding the Analysis tab for more detail) \nRule 1: Any month outside the control limits \nRule 2: Eight or more consecutive months all above, or all below, the centre line", size = 10) +
+    ggplot2::ylim(ylimlow, 100) +
+    ggplot2::geom_text(ggplot2::aes(label = ifelse(x==max(x), format(x, '%b-%y'),'')), hjust = -0.05, vjust = 2)
 
 
+  if(four_hr_plot == TRUE){
 
-   p <- plot$data
-
-   p_chart <- ggplot2::ggplot(p, ggplot2::aes(x,y)) +
-     ggplot2::geom_ribbon(ymin = p$lcl, ymax = p$ucl, fill = "grey",alpha = 0.4) +
-     ggplot2::geom_line(colour = "blue", size = .75) +
-     ggplot2::geom_line(ggplot2::aes(x,cl)) +
-     ggplot2::geom_point(colour = "black" , fill = "black", size = 1.5) +
-     ggplot2::ggtitle(label = "example i chart") +
-     ggplot2::labs(x = NULL,
-          y = NULL)+
-     ggplot2::theme_minimal()
-
-   p_chart
-
-   plot + ggplot2::geom_hline(yintercept = 0.95, colour = "red")
-
-
-
-  if(plot_chart == TRUE){
-
-    plot
+    four_hr_plot
 
   }else{
 
-    plot$data %>% select(Time, above_4hrs, under_4hrs, N)
+    four_hr_plot$pct %>% dplyr::select(Time, above_4hrs, under_4hrs, N)
 
   }
+
+}
+
+format_control_chart <- function(cht, r1_col, r2_col) {
+
+  point_colours <- c("Rule 1" = r1_col, "Rule 2" = r2_col, "None" = "black")
+  cht +
+    ggplot2::geom_line(colour = "black", size = 0.5) +
+    ggplot2::geom_line(ggplot2::aes(x,cl), size = 0.75) +
+    ggplot2::geom_line(ggplot2::aes(x,ucl), size = 0.75, linetype = 2) +
+    ggplot2::geom_line(ggplot2::aes(x,lcl), size = 0.75, linetype = 2) +
+    ggplot2::geom_point(ggplot2::aes(colour = highlight), size = 2) +
+    ggplot2::scale_color_manual("Rule triggered*", values = point_colours) +
+    ggplot2::theme(panel.grid.major.y = ggplot2::element_blank(),
+                   panel.grid.major.x = ggplot2::element_line(colour = "grey80"),
+                   panel.grid.minor = ggplot2::element_blank(), panel.background = ggplot2::element_blank(),
+                   axis.text.x = ggplot2::element_text(angle = 45, hjust = 1, vjust = 1.0, size = 14),
+                   axis.text.y = ggplot2::element_text(size = 14), axis.title = ggplot2::element_text(size = 14),
+                   plot.title = ggplot2::element_text(size = 20, hjust = 0),
+                   plot.subtitle = ggplot2::element_text(size = 16, face = "italic"),
+                   axis.line = ggplot2::element_line(colour = "grey60"),
+                   plot.caption = ggplot2::element_text(size = 10, hjust = 0.5))
 
 }
