88caea0072415302244bc4a827350057bf374521
Burcea <gburcea@ic.ac.uk>
2019-09-18 15:09:49 +0100

    filtered to only few groups since th

--- tests/testthat/test_weekly_ed_att_flgrp.R
+++ tests/testthat/test_weekly_ed_att_flgrp.R
@@ -12,6 +12,9 @@ test_that("Weekly ed attandances by flow groups",{
   result <- hospitalflow::weekly_ed_att_flgrp(start_date = as.Date("2016-01-01",tz = "Europe/London"), end_date = as.Date("2016-08-01",tz = "Europe/London"),
                                                                    data = weekly_ed_att_flgrp, time_unit = "weekly", plot_chart = FALSE, hospital_name = hospital_name)
 
+  result <- result %>%
+    dplyr::filter(flow_groups != "Flow A")
+
   #Test results are correct
   expect_equal(correct_answers, result)
 
