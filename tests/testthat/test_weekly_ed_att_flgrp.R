fb2b73b37862ec7d909a6e77f6bd3a26e6f79115
Burcea <gburcea@ic.ac.uk>
2019-09-26 16:40:23 +0100

    minor changes.

--- tests/testthat/test_weekly_ed_att_flgrp.R
+++ tests/testthat/test_weekly_ed_att_flgrp.R
@@ -9,7 +9,7 @@ test_that("Weekly ed attandances by flow groups",{
  correct_answers <- readRDS("testdata/weekly_ed_att_flgrps/correct_answers.rds")
 
 
-  result <- weekly_ed_att_flgrp(start_date = as.Date("2016-06-01",tz = "Europe/London"), end_date = as.Date("2016-06-30",tz = "Europe/London"),
+ result <- weekly_ed_att_flgrp(start_date = as.Date("2016-06-01",tz = "Europe/London"), end_date = as.Date("2016-06-30",tz = "Europe/London"),
                                 data = weekly_ed_att_flgrp, time_unit = "weekly", plot_chart = FALSE, hospital_name = hospital_name, restrict_plot_range = TRUE)
 
 
