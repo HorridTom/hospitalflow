27f2f0a4e71fd36fdaac0f794c7aeacc9031730e
Burcea <gburcea@ic.ac.uk>
2019-10-02 16:26:31 +0100

    minor changes - have filled the hospital_name with a string.

--- tests/testthat/test_weekly_ed_att_flgrp.R
+++ tests/testthat/test_weekly_ed_att_flgrp.R
@@ -4,13 +4,13 @@ library(hospitalflow)
 test_that("Weekly ed attandances by flow groups",{
 
 
- weekly_ed_att_flgrp <- readRDS("D:/Rprojects/hospitalflow/tests/testthat/testdata/weekly_ed_att_flgrps/weekly_ed_flgrp.rds")
+ weekly_ed_att_flgrp <- readRDS("testdata/weekly_ed_att_flgrps/weekly_ed_flgrp.rds")
 
  correct_answers <- readRDS("testdata/weekly_ed_att_flgrps/correct_answers.rds")
 
 
  result <- weekly_ed_att_flgrp(start_date = as.Date("2016-06-01",tz = "Europe/London"), end_date = as.Date("2016-06-30",tz = "Europe/London"),
-                                data = weekly_ed_att_flgrp, time_unit = "weekly", plot_chart = FALSE, hospital_name = hospital_name, restrict_plot_range = TRUE)
+                                data = weekly_ed_att_flgrp, time_unit = "weekly", plot_chart = FALSE, hospital_name = "hospital_name", restrict_plot_range = TRUE)
 
 
   #Test results are correct
