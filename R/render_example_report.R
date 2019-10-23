445ed0562c9c8ae4ec654057ad41ff9d2696cd75
HorridTom <woodcock.thomas@gmail.com>
2019-10-23 15:48:36 +0100

    Add parameter to example_report.Rmd to control number of weeks of sample data used. Also add function to render the example report and save output with useful filename.

---
+++ R/render_example_report.R
@@ -0,0 +1,18 @@
+#' render_example_report
+#'
+#' @param sample_weeks The number of weeks of sample data to use for the report.
+#' @return The system.time output for the time taken to render the report.
+#' @export
+#'
+#' @examples
+render_example_report <- function(sample_weeks = 4) {
+
+  system.time(rmarkdown::render(input = "vignettes/example-report.Rmd",
+                                output_format = "beamer_presentation",
+                                output_file = paste0("example-report_",
+                                                     gsub(":","-",paste(strsplit(x = toString(Sys.time()),split = " ")[[1]], collapse="-")),
+                                                     "_", stringr::str_sub(system("git rev-parse HEAD", intern=TRUE), 1, 8),
+                                                     ".pdf"),
+                                params = list(sample_weeks = sample_weeks)
+  ))
+}
