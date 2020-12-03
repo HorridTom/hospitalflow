419b6de27eaf37da5980c3c93b29568b1622605a
Imogen Connor-Helleur <imooconnor@outlook.com>
2020-12-03 13:06:57 +0000

    Added default to get_ward_mapping for development ease. Will remove when finished.

--- R/get_ward_mapping.r
+++ R/get_ward_mapping.r
@@ -1,5 +1,5 @@
 #function to get the broad specialism of a ward
-get_ward_mapping <- function(ward_name, config_path){
+get_ward_mapping <- function(ward_name, config_path = "lgt-config/inpatient/"){
   ward_type_mapping <- readRDS(file.path(config_path, "ward_type_mapping.rds"))
   getWardMapping <- ward_type_mapping$ward_broad_specialism
   names(getWardMapping) <- ward_type_mapping$ward
