35e8fab65a0787a1b243a6a31ea0a606eaf7745b
HorridTom <woodcock.thomas@gmail.com>
2021-10-19 17:15:13 +0100

    Allow output format to be specified in render_example_report

--- R/render_example_report.R
+++ R/render_example_report.R
@@ -5,15 +5,15 @@
 #' @export
 #'
 #' @examples
-render_example_report <- function(sample_weeks = 4,
+render_example_report <- function(output_format = NULL,
+                                  sample_weeks = 4,
                                   sample_no_of_patients = NA_integer_) {
 
   system.time(rmarkdown::render(input = "vignettes/example-report.Rmd",
-                                output_format = "beamer_presentation",
+                                output_format = output_format,
                                 output_file = paste0("example-report_",
                                                      gsub(":","-",paste(strsplit(x = toString(Sys.time()),split = " ")[[1]], collapse="-")),
-                                                     "_", stringr::str_sub(system("git rev-parse HEAD", intern=TRUE), 1, 8),
-                                                     ".pdf"),
+                                                     "_", stringr::str_sub(system("git rev-parse HEAD", intern=TRUE), 1, 8)),
                                 params = list(sample_weeks = sample_weeks,
                                               sample_no_of_patients = sample_no_of_patients)
   ))
