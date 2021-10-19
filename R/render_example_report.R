e842fddb27b243982d23b979515178b082d8e651
HorridTom <woodcock.thomas@gmail.com>
2021-10-19 16:07:21 +0100

    Add additional param to render_example_report

--- R/render_example_report.R
+++ R/render_example_report.R
@@ -5,7 +5,8 @@
 #' @export
 #'
 #' @examples
-render_example_report <- function(sample_weeks = 4) {
+render_example_report <- function(sample_weeks = 4,
+                                  sample_no_of_patients = NA_integer_) {
 
   system.time(rmarkdown::render(input = "vignettes/example-report.Rmd",
                                 output_format = "beamer_presentation",
@@ -13,6 +14,7 @@ render_example_report <- function(sample_weeks = 4) {
                                                      gsub(":","-",paste(strsplit(x = toString(Sys.time()),split = " ")[[1]], collapse="-")),
                                                      "_", stringr::str_sub(system("git rev-parse HEAD", intern=TRUE), 1, 8),
                                                      ".pdf"),
-                                params = list(sample_weeks = sample_weeks)
+                                params = list(sample_weeks = sample_weeks,
+                                              sample_no_of_patients = sample_no_of_patients)
   ))
 }
