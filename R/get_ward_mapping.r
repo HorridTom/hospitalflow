2313bc895aaeb2b130ef43c2c340086a5f20c7d5
Imogen Connor-Helleur <imooconnor@outlook.com>
2022-01-10 11:17:12 +0000

    Removed default from get_ward_mapping.

--- R/get_ward_mapping.r
+++ R/get_ward_mapping.r
@@ -7,7 +7,7 @@
 #' @export
 #'
 #' @examples
-get_ward_mapping <- function(ward_name, config_path = "lgt-config/inpatient/"){
+get_ward_mapping <- function(ward_name, config_path){
   ward_type_mapping <- readRDS(file.path(config_path, "ward_type_mapping.rds"))
   getWardMapping <- ward_type_mapping$ward_broad_specialism
   names(getWardMapping) <- ward_type_mapping$ward
