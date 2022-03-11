2bcb2227ae44c76396bad9951e7e3d84c4dfd659
Wartynas <55479800+Wartynas@users.noreply.github.com>
2022-03-11 19:28:38 +0000

    Replace ed_los_flow_grps by plot_ed_los_distribution_flow

--- tests/testthat/test_ed_los_flgrps.R
+++ tests/testthat/test_ed_los_flgrps.R
@@ -1,15 +1,16 @@
-context("ED length of stay for flow groups for real random data")
 
 test_that("ed length of stay for flow groups is calculated correctly", {
-  correct_answers <- readRDS("testdata/ed_los_flows/correct_answers.rds") %>%
+  correct_answers <- readRDS("testdata/plot_ed_los_distribution_flow/correct_answers.rds") %>%
     dplyr::ungroup()
 
-  ed_los_flow_groups <- readRDS("testdata/ed_los_flows/ed_los_flows.rds")
+  ed_los_flow_groups <- readRDS("testdata/plot_ed_los_distribution_flow/plot_ed_los_distribution_flow.rds")
 
-  result <- ed_los_flow_grps(
-    start_date = as.Date("2016-06-01", tz = "Europe/London"),
-    end_date = as.Date("2016-06-30", tz = "Europe/London"),
-    data = ed_los_flow_groups, plot_chart = FALSE, hospital_name = hospital_name
+  result <- plot_ed_los_distribution_flow(
+    data = ed_los_flow_groups,
+    startDate = as.Date("2016-06-01", tz = "Europe/London"),
+    endDate = as.Date("2016-06-30", tz = "Europe/London"),
+    returnPlot = FALSE,
+    hospitalName = "Anytown General Hospital"
   )
 
   # Test results are correct up to row order
