61a1a4cc2ca23e2327b26e775e313a35bcf43745
Wartynas <55479800+Wartynas@users.noreply.github.com>
2022-05-17 15:22:18 +0100

    switch some comments

--- R/spell_linkage.R
+++ R/spell_linkage.R
@@ -250,19 +250,19 @@ spell_variables <- function(all_episodes) {
 
   print("before reduce")
 
-  # episode_lists <- Reduce(function(...) dplyr::left_join(..., on = "spell_number"), lst_of_dfs)
+  episode_lists <- Reduce(function(...) dplyr::left_join(..., on = "spell_number"), lst_of_dfs)
 
-  episode_lists <- dplyr::left_join(const_episodes_df, gender_df)
-  episode_lists <- dplyr::left_join(episode_lists, age_band_df)
-  episode_lists <- dplyr::left_join(episode_lists, ep_class_seq_df)
-  episode_lists <- dplyr::left_join(episode_lists, admission_type_df)
-  episode_lists <- dplyr::left_join(episode_lists, datetime_df)
-  episode_lists <- dplyr::left_join(episode_lists, disposal_df)
-  episode_lists <- dplyr::left_join(episode_lists, hrg_df)
-  episode_lists <- dplyr::left_join(episode_lists, source_referral_df)
-  episode_lists <- dplyr::left_join(episode_lists, mortality_df)
+  # episode_lists <- dplyr::left_join(const_episodes_df, gender_df)
+  # episode_lists <- dplyr::left_join(episode_lists, age_band_df)
+  # episode_lists <- dplyr::left_join(episode_lists, ep_class_seq_df)
+  # episode_lists <- dplyr::left_join(episode_lists, admission_type_df)
+  # episode_lists <- dplyr::left_join(episode_lists, datetime_df)
+  # episode_lists <- dplyr::left_join(episode_lists, disposal_df)
+  # episode_lists <- dplyr::left_join(episode_lists, hrg_df)
+  # episode_lists <- dplyr::left_join(episode_lists, source_referral_df)
+  # episode_lists <- dplyr::left_join(episode_lists, mortality_df)
 
-  print("after reduce")
+  print("after reducee")
 
 
   spell_table <- all_episodes %>%
