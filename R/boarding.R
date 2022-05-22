4519ec02d1d1899cb77e19ef9e54706bacd5af97
Wartynas <55479800+Wartynas@users.noreply.github.com>
2022-05-22 17:38:20 +0100

    Add a check for the required variables

--- R/boarding.R
+++ R/boarding.R
@@ -1,6 +1,6 @@
 
 
-#' get_contingency_table
+#' Contingency table between ward category and main specialty
 #'
 #' @param spell_table
 #'
@@ -8,9 +8,17 @@
 #' @export
 #'
 #' @examples
-get_contigency_table <- function(spell_table){
+#'
+get_ward_specialty_contingency_table <- function(ip_data){
   # Perform check if the necessary column exist
-  # Include parameter "between". Use should input one of "PC", "PW", "cw"
+  if (!("ward_category" %in% colnames(ip_data))) {
+    stop("The 'ward_category' column not found in the provided dataframe.")
+  }
+
+  if (!("main_specialty" %in% colnames(ip_data))) {
+    stop("The 'main_specialty' column not found in the provided dataframe.")
+  }
+
   tbl <- as.data.frame.matrix(
     table(
       spell_table$ward_category, spell_table$main_specialty
